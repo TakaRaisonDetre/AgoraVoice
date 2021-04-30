bcfc03f0f55ccf4e917ea90cd0882f6bd476af82
原貴悌 <taka_raison@genkiteinoiMac.local>
2021-04-30 11:18:23 +0900

    agora voice chat app

---
+++ App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { SafeAreaView, View, Text, TextInput } from 'react-native';
+import { useInitializeAgora, useRequestAudioHook } from './src/hooks';
+import Button from './src/components/Button';
+import styles from './src/styles';
+
+
+ const App = () => {
+
+  useRequestAudioHook();
+  const {
+   channelName,
+   isMute,
+   isSpeakerEnable,
+   joinSucceed,
+   peerIds,
+   setChannelName,
+   joinChannel,
+   leaveChannel,
+   toggleIsMute,
+   toggleIsSpeakerEnable,
+  } = useInitializeAgora()
+
+
+   return (
+    
+ 
+        <SafeAreaView>
+         <View style={styles.container}>
+        <View style={styles.channelInputContainer}>
+          <Text>Enter Channel Name:</Text>
+
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => setChannelName(text)}
+            placeholder={'Channel Name'}
+            value={channelName}
+          />
+        </View>
+
+        <View style={styles.joinLeaveButtonContainer}>
+          <Button
+            onPress={joinSucceed ? leaveChannel : joinChannel}
+            title={`${joinSucceed ? 'Leave' : 'Join'} channel`}
+          />
+        </View>
+
+        <View style={styles.floatRight}>
+          <Button onPress={toggleIsMute} title={isMute ? 'UnMute' : 'Mute'} />
+        </View>
+
+        <View style={styles.floatLeft}>
+          <Button
+            onPress={toggleIsSpeakerEnable}
+            title={isSpeakerEnable ? 'Disable Speaker' : 'Enable Speaker'}
+          />
+        </View>
+
+        <View style={styles.usersListContainer}>
+          {peerIds.map((peerId) => {
+            return (
+              <View key={peerId}>
+                <Text>{`Joined User ${peerId}`}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </View>
+       </SafeAreaView> 
+   
+   );
+ };
+ 
+ export default App
---
+++ src/hooks.js
@@ -0,0 +1,121 @@
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { Platform } from 'react-native';
+import RtcEngine from 'react-native-agora';
+import { requestAudioPermission } from './permissions';
+
+export const useRequestAudioHook = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      // Request required permissions from Android
+
+      requestAudioPermission().then(() => {
+        console.log('requested!');
+      });
+    }
+  }, []);
+};
+
+export const useInitializeAgora = () => {
+  // Replace yourAppId with the App ID of your Agora project.
+  const appId = '7474aab9902548c4beddaff51c1f82d9';
+  const token =
+    '0066bb4e5cc2ba04652b76ba36a4b7b5ce6IABkVPtCpkjNEkV9DJO60ZxLuGq8iDC2cEIeKB1hxZNgp4a0dcYAAAAAEADr9jhXTbOMYAEAAQBNs4xg';
+
+  const [channelName, setChannelName] = useState('my-channel');
+  const [joinSucceed, setJoinSucceed] = useState(false);
+  const [peerIds, setPeerIds] = useState([]);
+  const [isMute, setIsMute] = useState(false);
+  const [isSpeakerEnable, setIsSpeakerEnable] = useState(true);
+  const rtcEngine = useRef(null);
+
+  const initAgora = useCallback(async () => {
+    rtcEngine.current = await RtcEngine.create(appId);
+
+    await rtcEngine.current?.enableAudio();
+    await rtcEngine.current?.muteLocalAudioStream(false);
+    await rtcEngine.current?.setEnableSpeakerphone(true);
+
+    rtcEngine.current?.addListener('UserJoined', (uid, elapsed) => {
+      console.log('UserJoined', uid, elapsed);
+
+      setPeerIds((peerIdsLocal) => {
+        if (peerIdsLocal.indexOf(uid) === -1) {
+          return [...peerIdsLocal, uid];
+        }
+
+        return peerIdsLocal;
+      });
+    });
+
+    rtcEngine.current?.addListener('UserOffline', (uid, reason) => {
+      console.log('UserOffline', uid, reason);
+
+      setPeerIds((peerIdsLocal) => {
+        return peerIdsLocal.filter((id) => id !== uid);
+      });
+    });
+
+    rtcEngine.current?.addListener(
+      'JoinChannelSuccess',
+      (channel, uid, elapsed) => {
+        console.log('JoinChannelSuccess', channel, uid, elapsed);
+
+        setJoinSucceed(true);
+
+        setPeerIds((peerIdsLocal) => {
+          return [...peerIdsLocal, uid];
+        });
+      },
+    );
+
+    rtcEngine.current?.addListener('Error', (error) => {
+      console.log('Join does not succeed', error);
+    });
+  }, []);
+
+  const joinChannel = useCallback(async () => {
+    await rtcEngine.current?.joinChannel(token, channelName, null, 0);
+  }, [channelName]);
+
+  const leaveChannel = useCallback(async () => {
+    await rtcEngine.current?.leaveChannel();
+
+    setPeerIds([]);
+    setJoinSucceed(false);
+  }, []);
+
+  const toggleIsMute = useCallback(async () => {
+    await rtcEngine.current?.muteLocalAudioStream(!isMute);
+    setIsMute(!isMute);
+  }, [isMute]);
+
+  const toggleIsSpeakerEnable = useCallback(async () => {
+    await rtcEngine.current?.setEnableSpeakerphone(!isSpeakerEnable);
+    setIsSpeakerEnable(!isSpeakerEnable);
+  }, [isSpeakerEnable]);
+
+  const destroyAgoraEngine = useCallback(async () => {
+    await rtcEngine.current?.destroy();
+  }, []);
+
+  useEffect(() => {
+    initAgora();
+
+    return () => {
+      destroyAgoraEngine();
+    };
+  }, [destroyAgoraEngine, initAgora]);
+
+  return {
+    channelName,
+    isMute,
+    isSpeakerEnable,
+    joinSucceed,
+    peerIds,
+    setChannelName,
+    joinChannel,
+    leaveChannel,
+    toggleIsMute,
+    toggleIsSpeakerEnable,
+  };
+};
